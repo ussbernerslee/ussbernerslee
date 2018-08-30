0a85ff50308e0e7ee43955cd39fd6e5924342796
Dylan McDonald <dmcdonald21@cnm.edu>
2018-08-29 22:40:02 -0600

    added deploy with AWS placeholders

---
+++ bin/aws-deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# application details
+version="1.0.0"
+app_name="USS Berners-Lee"
+
+# aws credentials
+access_key=YOUR_KEY
+access_secret=YOUR_SECRET
+
+# values
+timestamp=$(date +%s)
+
+# Log off current machine
+eval $(docker-machine env -u)
+
+# Create machine on aws
+docker-machine create \
+	--driver amazonec2 \
+	--amazonec2-access-key $access_key \
+	--amazonec2-secret-key $access_secret \
+	--amazonec2-open-port 80 \
+	--amazonec2-open-port 443 \
+$app_name-instance-$timestamp
+
+# set context to aws machine, every subsequent docker action
+# is run agains this daemon
+eval $(docker-machine env $app_name-instance-$timestamp)
+
+# Get the IP address of new EC2 instance
+# Copy this IP and add an A record to the hosted zone
+EC2IP=$(docker-machine ip $app_name-instance-$timestamp)
+
+echo "EC2 instance public address: $EC2IP"
+
+# Create image on EC2
+docker build --rm -t $app_name:$version .
+
+# Run new container
+docker run -d -p 80:80 -p 443:443 $app_name:$version
